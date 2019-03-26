7b84e6679e97fa272b1e4527ac7e0f592d23bfec
arvind <raghavan.arvind@gmail.com>
2019-03-26 12:25:30 -0500

    added random points

--- star_runner.py
+++ star_runner.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 import os, subprocess
 import rospy, rosgraph
-import time
+import random
 from bwi_tools import start_roslaunch_process, stop_roslaunch_process
 
 import std_msgs.msg
 from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
 from bwi_msgs.srv import DoorHandlerInterface
+
+POINTS = [(-1.77, 104.77),
+          (-1.98, 112.47),
+          (23.73, 109.22),
+          (51.25, 113.38),
+          (52.88, 106.30),
+          (37.00, 101.64),
+          (34.92, 108.36),
+          (44.84, 108.27)]
+
 
 def roscore():
     subprocess.Popen('roscore')
@@ -37,10 +47,10 @@ def star_publisher():
     try:
         process = start_roslaunch_process("bwi_launch", "simulation_v2.launch")
         pub = rospy.Publisher('/move_base_interruptable_simple/goal', PoseStamped, queue_size=3)
-        time.sleep(30)
+        rospy.sleep(25)
 
         open_all_doors()
-        pub.publish(make_pose_stamped(8, 108))
+        pub.publish(make_pose_stamped(*random.choice(POINTS)))
 
         rospy.spin()
 
